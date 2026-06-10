6c6622ded7ae657db173ffa1007cb831e19cba75
Harinath <ontigariharinath123@gmail.com>
2026-06-10 23:19:28 +0530

    3rd commit added

--- src/main/java/demo/BankAccount.java
+++ src/main/java/demo/BankAccount.java
@@ -7,6 +7,7 @@ public class BankAccount {
     private final double balance;
 //this is the constructor for instance members initialization
     //we using accountNumber, accountHolder, and balance details
+    //it is 3rd line comment for git
     public BankAccount(int accountNumber,
                        String accountHolder,
                        double balance) {
